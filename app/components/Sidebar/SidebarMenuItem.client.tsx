167ad4d7d753a73e9e31751bbd74737ea2cb164c
Raymall Perez <raymall.perez@gmail.com>
2025-01-13 19:07:16 -0400

    Add Sidebar component

---
+++ app/components/Sidebar/SidebarMenuItem.client.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { Home, Users } from 'lucide-react'
+import { 
+  SidebarMenu as AppSidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from '@/components/ui/sidebar'
+import { usePathname } from 'next/navigation'
+
+export const SidebarMenu = ({
+  user_permissions,
+  organization_handle
+}: {
+  user_permissions: string[]
+  organization_handle: string
+}) => {
+
+  const items = [
+    {
+      title: 'Home',
+      url: `/${organization_handle}/dashboard`,
+      is_active: usePathname().includes('/dashboard'),
+      icon: Home,
+    },
+    {
+      title: 'Users',
+      url: `/${organization_handle}/users`,
+      is_active: usePathname().includes('/users'),
+      icon: Users,
+      permissions: ['read:users'],
+    },
+  ]
+
+  return (
+    <AppSidebarMenu>
+      {items.map((item) => {
+        'use client'
+
+        if (
+          item.permissions &&
+          !item.permissions.some((permission) => user_permissions?.includes(permission))
+        ) {
+          return
+        }
+        
+        return (
+          <SidebarMenuItem key={item.title}>
+            <SidebarMenuButton
+              asChild
+              isActive={item.is_active}
+            >
+              <a href={item.url}>
+                <item.icon />
+                <span>{item.title}</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        )
+      }
+    )}
+    </AppSidebarMenu>
+  )
+}
---
+++ app/components/Sidebar/index.tsx
@@ -0,0 +1,99 @@
+import slugify from 'slugify'
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components'
+import { ChevronsUpDown, Check } from 'lucide-react'
+import {
+  Sidebar as AppSidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarFooter,
+} from '@/components/ui/sidebar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import {
+  getIsUserAuthenticated,
+  getUser,
+  getUserPermissions,
+  getUserOrganizationName,
+  getUserOrganizations,
+} from '@/app/actions'
+import { LogoutButton } from '@/app/components/LogoutButton'
+import { SidebarMenu } from './SidebarMenuItem.client'
+import styles from './styles.module.scss'
+
+export const Sidebar = async () => {
+  const is_user_authenticated = await getIsUserAuthenticated()
+  const user = await getUser()
+  const user_permissions = (await getUserPermissions()) || []
+  const organization_name = await getUserOrganizationName() || ''
+  const organizations = await getUserOrganizations() || []
+  const organization_handle = slugify(organization_name || '', { lower: true })
+
+  return (
+    <AppSidebar className={styles.Sidebar}>
+      {/* <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader> */}
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu
+              user_permissions={user_permissions}
+              organization_handle={organization_handle}
+            />
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      {is_user_authenticated && (
+        <SidebarFooter>
+          <SidebarGroupLabel>
+            {user ? 
+              <span>{user.email}</span>
+            : null}
+          </SidebarGroupLabel>
+          {organizations && organizations.length > 1 ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant={'outline'} className={styles.Sidebar_organization_dropdown}>
+                  {organization_name}
+                  <ChevronsUpDown className="ml-auto" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {organizations && organizations.map((organization) => (
+                  <DropdownMenuItem key={organization.code}>
+                    {organization_name === organization.name && (
+                      <Check />
+                    )}
+                    <LoginLink orgCode={organization.code} className={styles.Sidebar_organization_signin}>
+                      {organization.name}
+                    </LoginLink>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ): (
+            <SidebarGroupLabel>
+              <span>{organization_name}</span>
+            </SidebarGroupLabel>
+          )}
+          <LogoutButton className={styles.Sidebar_logout_button} />
+        </SidebarFooter>
+      )}
+    </AppSidebar>
+  )
+}
