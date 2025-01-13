b6477f81ae9313138b9ddbc057bb1578d07b9cd9
Raymall Perez <raymall.perez@gmail.com>
2025-01-13 19:05:24 -0400

    Setup next.js

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import { AuthProvider } from './AuthProvider'
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.scss'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'auth-starter',
+  description: 'Template',
+}
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <AuthProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,66 @@
+import slugify from 'slugify'
+import { redirect } from 'next/navigation'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { LoginLink, RegisterLink, LogoutLink } from '@kinde-oss/kinde-auth-nextjs/components'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import { LogIn, UserRoundPlus, LogOut } from 'lucide-react'
+import styles from './page.module.scss'
+
+export default async function Home() {
+  const { isAuthenticated, getOrganization } = getKindeServerSession()
+  const is_user_authenticated = await isAuthenticated()
+  const organization_name = (await getOrganization())?.orgName
+  
+  if (is_user_authenticated && organization_name) {
+    const organization_handle = slugify(organization_name, { lower: true })
+    redirect(`/${organization_handle}/dashboard`)
+  }
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1 className={styles.Title}>[auth-starter]</h1>
+        <Card>
+          <CardHeader>
+            <CardTitle>Welcome!</CardTitle>
+          </CardHeader>
+          {!is_user_authenticated && !organization_name ? (
+            <CardFooter className={styles.CardFooter}>
+              <LoginLink className={styles.CardFooter_link}>
+                <Button className={styles.CardFooter_button}>
+                  <LogIn /> Login
+                </Button>
+              </LoginLink>
+              
+              <RegisterLink className={styles.CardFooter_link}>
+                <Button className={styles.CardFooter_button}>
+                  <UserRoundPlus /> Sign Up
+                </Button>
+              </RegisterLink>
+            </CardFooter>
+          ) : (
+            <>
+              <CardContent>
+                <p>You need to be added to an organization.</p>
+              </CardContent>
+              <CardFooter>
+                <LogoutLink>
+                  <Button>
+                    <LogOut /> Log Out
+                  </Button>
+                </LogoutLink>
+              </CardFooter>
+            </>
+          )}
+        </Card>
+      </main>
+    </div>
+  )
+}
